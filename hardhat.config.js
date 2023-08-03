fc45c645415cd82d93cbcbd03c0925a5a7ff4d5d
Aniket <aniketmessi3@gmail.com>
2023-08-03 23:57:26 +0530

    rest commits

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+// require("@nomiclabs/hardhat-waffle");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+// console.log(process.env);
 module.exports = {
   solidity: "0.8.18",
+  networks : {
+    goerli : {
+      url : "https://eth-goerli.g.alchemy.com/v2/X0fNvHIcsHTODGULv2l0U3zQrJYLHnzg",
+      accounts : process.env.TESTNET_PRIVATE_KEY
+  },
+  }
 };
